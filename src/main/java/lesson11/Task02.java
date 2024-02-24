37e2eb6f6cda71e29d669d02819d5488970f9c2c
AnastasiaCherkashyna <160175625+AnastasiaCherkashyna@users.noreply.github.com>
2024-02-24 16:32:10 +0100

    Init commit CalculateAreaAndPerimeter

---
+++ src/main/java/lesson11/Task02.java
@@ -0,0 +1,15 @@
+package lesson11;
+
+import java.util.Enumeration;
+
+public class Task02 {
+    public static void main(String[] args) {
+        boolean result = isEven(90);
+        System.out.println(result);
+        
+    }
+    public static boolean isEven (int number) {
+            return number % 2 == 0;
+        }
+    }
+
