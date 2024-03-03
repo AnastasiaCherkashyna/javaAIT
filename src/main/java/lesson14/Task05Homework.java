ed3a417b142df095618fe017de4334ab1aca90ac
AnastasiaCherkashyna <160175625+AnastasiaCherkashyna@users.noreply.github.com>
2024-03-03 07:41:10 +0100

    Task 05 Hobby

---
+++ src/main/java/lesson14/Task05Homework.java
@@ -0,0 +1,37 @@
+package lesson14;
+
+public class Task05Homework {
+    public static void main(String[] args) {
+        // First Array
+        String[] hobbies = new String[4];
+        hobbies[0] = "traveling";
+        hobbies[1] = "reading books";
+        hobbies[2] = "swimming";
+        hobbies[3] = "playing piano";
+
+        String[] hobbiesNew = new String[2];
+        hobbiesNew[0] = "Cooking";
+        hobbiesNew[1] = "Walking";
+
+
+        String[] allHobbies = new String[6];
+        for (int i = 0; i < hobbies.length; i++) {
+            allHobbies[i] = hobbies[i];
+        }
+        for (int i = 0; i < hobbiesNew.length; i++) {
+            allHobbies[i + hobbies.length] = hobbiesNew[i];
+        }
+        System.out.println("Полный список ваших хобби: ");
+        for (String all : allHobbies)
+            System.out.println(all);
+
+
+         /* String [] all ( String[]hobbies, String []hobbiesNew){
+            String[] allHobbies = new String[hobbies.length + hobbiesNew.length];
+            System.arraycopy(hobbies, 0, allHobbies, 0, hobbies.length);
+            System.arraycopy(hobbiesNew, 0, allHobbies, hobbies.length, hobbiesNew.length);
+            return allHobbies;
+        } */
+    }
+}
+
