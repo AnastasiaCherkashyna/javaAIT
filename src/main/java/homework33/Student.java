c2486768873d5fee5f818d0ba3966a81fb5b26d6
AnastasiaCherkashyna <160175625+AnastasiaCherkashyna@users.noreply.github.com>
2024-05-25 12:18:57 +0200

    Homework33

---
+++ src/main/java/homework33/Student.java
@@ -0,0 +1,21 @@
+package homework33;
+
+import java.util.HashSet;
+
+public class Student {
+    private String name;
+    private int grade;
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+}
